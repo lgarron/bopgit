c0dcbdd513c20b098587e33ff998ae86aefa6b7c
Lucas Garron <code@garron.net>
2019-05-16 22:33:39 -0700

    Add plumbing for `set` command.

--- main.go
+++ main.go
@@ -6,38 +6,65 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func showHelp() {
-	flag.Usage()
+	fmt.Println(`Usage:
+    set [baseRef]
+    set [branch] [baseRef]
+`)
 	os.Exit(0)
+}
+
+func mustHaveMinNArgs(n int) {
+	if flag.NArg() < n {
+		showHelp()
+	}
 }
 
 func main() {
 	flag.Parse()
-	if flag.NArg() < 1 {
-		showHelp()
-	}
+	mustHaveMinNArgs(1)
+
 	switch flag.Arg(0) {
 	case "help":
 		showHelp()
 	case "set":
-		set(flag.Arg(1))
+		setCmd()
 	}
 }
 
-func runGitCommand(args ...string) {
-	out, err := exec.Command("git", args...).Output()
+func runGitCommand(args ...string) string {
+	output, err := exec.Command("git", args...).Output()
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
+	return strings.TrimSuffix(string(output), "\n")
 }
 
-func currentBranch(): string {
-  runGitCommand("rev-parse", "--abbrev-ref", "HEAD")
+func currentBranch() string {
+	return runGitCommand("rev-parse", "--abbrev-ref", "HEAD")
 }
 
-func set(baseRef string) {
-	fmt.Println(currentBranch(), baseRef)
+func bopgitRefName(branch string) string {
+	return fmt.Sprintf("refs/bopgit/%s", branch)
+}
+
+func setCmd() {
+	if flag.NArg() == 2 {
+		set(currentBranch(), flag.Arg(1))
+	} else if flag.NArg() == 3 {
+		set(flag.Arg(1), flag.Arg(2))
+	} else {
+		showHelp()
+	}
+}
+
+func set(branch string, baseRef string) {
+	fmt.Printf("Setting the base branch for %s to %s", branch, baseRef)
+
+	fmt.Println("update-ref", bopgitRefName(currentBranch()), baseRef)
+	// fmt.Println(currentBranch(), baseRef)
 }
